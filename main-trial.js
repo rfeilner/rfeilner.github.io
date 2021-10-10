b3663a0a553dd4b82ffa39c462fd8bf15d8e1383
rfeilner <87293262+rfeilner@users.noreply.github.com>
2021-10-10 14:24:21 -0500

    Update main-trial.js

--- main-trial.js
+++ main-trial.js
@@ -48,17 +48,3 @@ $( window ).resize( function() {
 //	});
 //};
 //$.fn.fade_cycle()
-
-var pos = 0;
-$(window).bind('mousewheel DOMMouseScroll', function(event) {
-  alert("!");
-  if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-    pos = pos + 50;
-  } else {
-    if (pos > 1) {
-
-      pos = pos - 50;
-    }    
-  }
-  $('#yscroll').scrollLeft(pos)    
-});
